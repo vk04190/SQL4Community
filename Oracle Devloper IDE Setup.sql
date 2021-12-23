e609d97da66f4a799f4ea0c2431576abc3aac3fe
Vivek Kumar <vivekkumar.xda@gmail.com>
2021-12-24 01:23:54 +0530

    updated extension names

--- Oracle Devloper IDE Setup.sql
+++ Oracle Devloper IDE Setup.sql
@@ -1,19 +1,27 @@
-1. Install Ubantu 16.04.3 With Dual Boot
-2. boot Into Ubantu Enviroment
-3. Install vs code ide
+1. Install vs code ide
     a. download from vs code from Microsoft Devloper website
     a. Add your Working directory
-    b. add extension following 
+    b. ADD FOLLOWING EXTENSION SPECIFIC TO SQL/PLSQL
         1.mssql
         2.oracle-plsql
         3.Intellisense for the oracle Devloper
-        4.Language plsql
+        4. Language plsql
         5. Language plsql
         6. responsys built-in function
-        7. pl/sql
-        8. sqr
-        9.odb-task
-        10. vscode-icons
+        7. PL/SQL
+        8. sql
+        9. odb-task
+    C INSTALL ADDITIONAL USEFULL EXTENSION WHICH WILL HELP WHILE CODING IN VSCODE
+        1. vscode-icons
+        2. Bracket Pair Colorizer 2
+        3. Code Spell Checker
+        4. file-size
+        5. Git Extension Pack
+        6. indent-rainbow
+        7. Indenticator
+        8. Visual Studio IntelliCode
+        9. Settings Sync
+        10. vscode-pdf
 
 4. Install git using "sudo apt-Install git"
-5.
+5
